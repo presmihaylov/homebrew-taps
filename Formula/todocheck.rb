63628398760b0641d35e16e6d24e29400d6e546c
preslavmihaylov <preslav@pmihaylov.com>
2022-02-12 17:55:50 +0200

    publish latest todocheck version

--- Formula/todocheck.rb
+++ Formula/todocheck.rb
@@ -3,9 +3,9 @@
 class Todocheck < Formula
   desc "A static-code analyzer, ensuring your TODOs are linked to your issue tracker"
   homepage "https://github.com/preslavmihaylov/todocheck"
-  url "https://github.com/preslavmihaylov/todocheck/archive/refs/tags/v0.3.0.zip"
-  version "0.3.0"
-  sha256 "297138f2d7a534b64531e1eff4f314c26da74c14a217052326189ae10f493a23"
+  url "https://github.com/preslavmihaylov/todocheck/archive/refs/tags/v0.5.0.zip"
+  version "0.5.0"
+  sha256 "b6ef774f5f13a6aaa57188e49bd7171897cf62739632c19f12b2ca74bfd94ee5"
   license "MIT"
 
   depends_on "go" => :build
