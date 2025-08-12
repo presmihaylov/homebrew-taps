7d2a77410f23398b17f1e8e20a99b54b3a9da3b7
preslavmihaylov <preslav@pmihaylov.com>
2025-08-12 11:08:35 +0300

    add latest ccagent binary

--- Formula/ccagent.rb
+++ Formula/ccagent.rb
@@ -7,20 +7,24 @@ class Ccagent < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/presmihaylov/ccagent/releases/download/v#{version}/ccagent-v#{version}-darwin-arm64"
-      sha256 "REPLACE_WITH_ARM64_SHA256"
+      sha256 "Getting SHA256 for ccagent-v0.0.3-darwin-arm64...
+0880333b6880773174cbbf81aab4a9709b000ba5e60b0fb625553ce66f24f3ac"
     else
       url "https://github.com/presmihaylov/ccagent/releases/download/v#{version}/ccagent-v#{version}-darwin-x86_64"
-      sha256 "REPLACE_WITH_X86_64_SHA256"
+      sha256 "Getting SHA256 for ccagent-v0.0.3-darwin-x86_64...
+46a575f5ec512e5ed5d0be2ab32ded02f76b5de34feeb8e621e3e2f02ea12d40"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/presmihaylov/ccagent/releases/download/v#{version}/ccagent-v#{version}-linux-arm64"
-      sha256 "REPLACE_WITH_LINUX_ARM64_SHA256"
+      sha256 "Getting SHA256 for ccagent-v0.0.3-linux-arm64...
+737f08a066999a3d726302092a77ac5f1fa1d7c49295ac346f3a3e9a10c0d53e"
     else
       url "https://github.com/presmihaylov/ccagent/releases/download/v#{version}/ccagent-v#{version}-linux-x86_64"
-      sha256 "REPLACE_WITH_LINUX_X86_64_SHA256"
+      sha256 "Getting SHA256 for ccagent-v0.0.3-linux-x86_64...
+49f7be541474825121d51f18bf65e0ff3ac562c1069262ade8dceaddfb4f7e11"
     end
   end
 
