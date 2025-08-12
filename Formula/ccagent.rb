c0e1a8bb46f17b65043b132f60a889cb26d11def
preslavmihaylov <preslav@pmihaylov.com>
2025-08-12 11:08:02 +0300

    add ccagent tap

---
+++ Formula/ccagent.rb
@@ -0,0 +1,36 @@
+class Ccagent < Formula
+  desc "CLI agent for Claude Code integration with Socket.IO backend"
+  homepage "https://github.com/presmihaylov/ccagent"
+  license "MIT"
+  version "0.0.3"
+  
+  on_macos do
+    if Hardware::CPU.arm?
+      url "https://github.com/presmihaylov/ccagent/releases/download/v#{version}/ccagent-v#{version}-darwin-arm64"
+      sha256 "REPLACE_WITH_ARM64_SHA256"
+    else
+      url "https://github.com/presmihaylov/ccagent/releases/download/v#{version}/ccagent-v#{version}-darwin-x86_64"
+      sha256 "REPLACE_WITH_X86_64_SHA256"
+    end
+  end
+
+  on_linux do
+    if Hardware::CPU.arm?
+      url "https://github.com/presmihaylov/ccagent/releases/download/v#{version}/ccagent-v#{version}-linux-arm64"
+      sha256 "REPLACE_WITH_LINUX_ARM64_SHA256"
+    else
+      url "https://github.com/presmihaylov/ccagent/releases/download/v#{version}/ccagent-v#{version}-linux-x86_64"
+      sha256 "REPLACE_WITH_LINUX_X86_64_SHA256"
+    end
+  end
+
+  def install
+    # The downloaded file will have the full name, rename to just 'ccagent'
+    downloaded_file = Dir["ccagent-*"].first
+    bin.install downloaded_file => "ccagent"
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/ccagent --version")
+  end
+end
