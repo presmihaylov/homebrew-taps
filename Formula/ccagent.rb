ba958fe44f400195bc841acd4c4937ff2123e72f
Preslav Mihaylov <preslav@pmihaylov.com>
2025-12-24 11:33:12 +0000

    Update ccagent to v0.0.27

--- Formula/ccagent.rb
+++ Formula/ccagent.rb
@@ -2,25 +2,25 @@ class Ccagent < Formula
   desc "CLI agent for Claude Code integration with Socket.IO backend"
   homepage "https://github.com/presmihaylov/ccagent"
   license "MIT"
-  version "0.0.26"
-  
+  version "0.0.27"
+
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/presmihaylov/ccagent/releases/download/v#{version}/ccagent-v#{version}-darwin-arm64"
-      sha256 "22c91d6be8e74d4e0a26430df609e2815bfbad1d80cc181905ecab91a85337c9"
+      url "https://github.com/presmihaylov/ccagent/releases/download/#{version}/ccagent-#{version}-darwin-arm64"
+      sha256 "93f949812dc84003df79c11f2434b934c23498405667a451909012eec77327f2"
     else
-      url "https://github.com/presmihaylov/ccagent/releases/download/v#{version}/ccagent-v#{version}-darwin-x86_64"
-      sha256 "294edb6a1885c39f1de358326ba877e1ed6f08bb8d06e761c02c4d7531b5a4ad"
+      url "https://github.com/presmihaylov/ccagent/releases/download/#{version}/ccagent-#{version}-darwin-x86_64"
+      sha256 "a9a6795e16c2524e98b47cc81fc21bee8cac6b0419a59a51dc76b22fe06d7e9f"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/presmihaylov/ccagent/releases/download/v#{version}/ccagent-v#{version}-linux-arm64"
-      sha256 "450a69d69610be623f644768ae8fd955e95fbb10978d9d3cfcc58e3ea7d0bfdf"
+      url "https://github.com/presmihaylov/ccagent/releases/download/#{version}/ccagent-#{version}-linux-arm64"
+      sha256 "ab16a85b134e590b7bd67572263199fb5a851d080e5e8248eda948e6d35eed27"
     else
-      url "https://github.com/presmihaylov/ccagent/releases/download/v#{version}/ccagent-v#{version}-linux-x86_64"
-      sha256 "41d4e5b364c008ab3abad8d9b4c93f62ad75e74128362c03c0e1676221647537"
+      url "https://github.com/presmihaylov/ccagent/releases/download/#{version}/ccagent-#{version}-linux-x86_64"
+      sha256 "8889daf4ad6a3fd10acff9325ebdf6d3939f4bdfcb3c03daf7735ceb5d3a5da1"
     end
   end
 
